77fb41b7467c3a9c39080791bfc308da708ff491
Tsiskomichelis Stelios <tsiskost@gmail.com>
2019-10-24 15:55:17 +0300

    Data initilization

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DataInitializer.java
@@ -0,0 +1,62 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initializeDummyData();
+    }
+
+    private void initializeDummyData(){
+
+        Publisher p1 = new Publisher("George Allen & Unwin");
+        publisherRepository.save(p1);
+
+        Author a1 =  new Author("J. R. R.", "Tolkien");
+        Book b1 = new Book("The Silmarillion", "0-04-823139-8",p1);
+
+
+        a1.getBooks().add(b1);
+        authorRepository.save(a1);
+
+        b1.getAuthors().add(a1);
+        bookRepository.save(b1);
+
+
+        Publisher p2 = new Publisher("Secker & Warburg");
+        publisherRepository.save(p2);
+
+        Author a2 =  new Author("George", "Orwell");
+        Book b2 = new Book("Nineteen Eighty-Four: A Novel", "470015866",p2);
+
+
+        a2.getBooks().add(b2);
+        authorRepository.save(a2);
+
+        b2.getAuthors().add(a2);
+        bookRepository.save(b2);
+
+
+    }
+}
